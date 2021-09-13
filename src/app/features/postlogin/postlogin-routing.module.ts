e16e67617bbd9204e275d40c9a25ca5705eeb608
siddharth0-0 <agrawalsid05@gmail.com>
2021-09-13 23:48:02 +0530

    preview-start

--- src/app/features/postlogin/postlogin-routing.module.ts
+++ src/app/features/postlogin/postlogin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InformationModule } from './information/information.module';
 import { PostHomeComponent } from './post-home/post-home.component';
+import { PreviewComponent } from './preview/preview.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: 'homepage', component: PostHomeComponent },
+  { path: 'preview', component: PreviewComponent },
   { path: 'information',  loadChildren: () => InformationModule},
 ];
 
--- src/app/features/postlogin/postlogin.module.ts
+++ src/app/features/postlogin/postlogin.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PostloginRoutingModule } from './postlogin-routing.module';
 import { PostHomeComponent } from './post-home/post-home.component';
+import { PreviewComponent } from './preview/preview.component';
 
 
 @NgModule({
-  declarations: [PostHomeComponent],
+  declarations: [PostHomeComponent, PreviewComponent],
   imports: [
     CommonModule,
     PostloginRoutingModule
